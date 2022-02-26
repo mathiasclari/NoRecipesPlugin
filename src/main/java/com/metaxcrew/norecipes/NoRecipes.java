9d3918be7b530281caa78504a6749b03b38a592e
Mat <silencekrakens@gmail.com>
2022-02-26 18:53:21 +0100

    Update

--- src/main/java/com/metaxcrew/norecipes/NoRecipes.java
+++ src/main/java/com/metaxcrew/norecipes/NoRecipes.java
@@ -13,11 +13,6 @@ public final class NoRecipes extends JavaPlugin {
         Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "[]=====================<" + ChatColor.of(new Color(169, 45, 81)) + ChatColor.BOLD + "MetaxCrew" + ChatColor.GRAY + ">=====================[]");
         Bukkit.getConsoleSender().sendMessage(ChatColor.of(new Color(0, 141, 183))+"");
         Bukkit.getConsoleSender().sendMessage(ChatColor.of(new Color(69, 72, 77)) + "    ・ Plugin:" + ChatColor.of(new Color(0, 141, 183)) + " NoRecipes");
-        Bukkit.getConsoleSender().sendMessage(ChatColor.of(new Color(69, 72, 77)) + "    ・ Developer:" + ChatColor.of(new Color(0, 141, 183)) + " Mat");
-        Bukkit.getConsoleSender().sendMessage(ChatColor.of(new Color(69, 72, 77)) + "    ・ Discord:" + ChatColor.of(new Color(0, 141, 183)) + " Mat#5000");
-        Bukkit.getConsoleSender().sendMessage(ChatColor.of(new Color(0, 141, 183))+"");
-        Bukkit.getConsoleSender().sendMessage(ChatColor.of(new Color(69, 72, 77)) + "    ・ Category:" + ChatColor.of(new Color(0, 141, 183)) + " Px");
-        Bukkit.getConsoleSender().sendMessage(ChatColor.of(new Color(0, 141, 183))+"");
         Bukkit.getConsoleSender().sendMessage(ChatColor.of(new Color(69, 72, 77)) + "    ・ Support Discord:" + ChatColor.of(new Color(0, 141, 183)) + " https://discord.gg/gFmBdpPZJa");
         Bukkit.getConsoleSender().sendMessage(ChatColor.of(new Color(0, 141, 183))+"");
         Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "[]====================================================[]");
